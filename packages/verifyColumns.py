a71893f4f7e11dcef2bd9645b6fabc9fb45add53
HyBun <davipare203@gmail.com>
2023-07-06 00:39:11 -0300

    fix: fix the kwargs update

--- packages/verifyColumns.py
+++ packages/verifyColumns.py
@@ -1,19 +1,26 @@
+import inspect
+
 import pandas as pd
 
-estados = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI',
+estados = ['AR', 'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE',
+           'PI',
            'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
 
 
 def verifyuf(func):
     def wrapper(*args, **kwargs):
-        # Verifica se são 6 ou mais parametros e se os ultimos dois estão com o valor None
-        if len(args) >= 6 and args[-2:] == (None, None):
-            args = list(args)
-            sheetorigem = pd.read_excel(args[0])  # Path para a planilha que contém as cidade de origem 
-            sheetdestino = pd.read_excel(args[1])  # Path para a planilha que contém as cidade de destino
+        """
 
-            colunaorigem = args[2]  # Nome da coluna que possui as cidades de origem
-            colunadestino = args[3]  # Nome da coluna que possui as cidades de destino
+        :type kwargs: object
+        """
+        # Verifica se são 6 ou mais parametros e se os ultimos dois estão com o valor None
+        if args[-2:] == (None, None):
+            args = list(args)
+            sheetorigem = pd.read_excel(args[1])  # Path para a planilha que contém as cidade de origem
+            sheetdestino = pd.read_excel(args[2])  # Path para a planilha que contém as cidade de destino
+
+            colunaorigem = args[3]  # Nome da coluna que possui as cidades de origem
+            colunadestino = args[4]  # Nome da coluna que possui as cidades de destino
 
             # Dados para consulta da planilha de Origem
             coluna_lado = sheetorigem.columns.get_loc(colunaorigem) + 1  # Pega a posição da coluna do lado
@@ -28,43 +35,45 @@ def verifyuf(func):
             try:
                 # Verifica se o valor pego na coluna possivelmente UF é um estado.
                 if estado_consulta.upper() in estados:
-                    args[4] = coluna_lado_nome
+                    args[5] = coluna_lado_nome
 
                 # Verifica se o valor pego na coluna possivelmente UF é um estado.
                 if estado_consulta_destino.upper() in estados:
-                    args[5] = coluna_lado_nome_destino
+                    args[6] = coluna_lado_nome_destino
 
             except Exception as e:
                 print(e)
 
         elif kwargs.get('uforigem') is None and kwargs.get('ufdestino') is None:
-            sheetorigem = pd.read_excel(kwargs['sheet1'])
-            sheetdestino = pd.read_excel(kwargs['sheet2'])
+            sheetorigem = pd.read_excel(kwargs.get('sheet1'))
+            sheetdestino = pd.read_excel(kwargs.get('sheet2'))
 
-            colunaorigem = kwargs['colunaorigem']
-            colunadestino = kwargs['colunadestino']
+            colunaorigem = kwargs.get('colunaorigem')
+            colunadestino = kwargs.get('colunadestino')
 
             # Dados para consulta da planilha de Origem
             coluna_lado = sheetorigem.columns.get_loc(colunaorigem) + 1
-            coluna_lado_nome = sheetorigem.columns[coluna_lado]
+            coluna_lado_nome = sheetorigem.columns.values[coluna_lado]
             estado_consulta = sheetorigem.loc[0, coluna_lado_nome]
 
             # Dados para consulta da planilha de destino
             coluna_lado_destino = sheetdestino.columns.get_loc(colunadestino) + 1
-            coluna_lado_nome_destino = sheetdestino.columns[coluna_lado_destino]
+            coluna_lado_nome_destino = sheetdestino.columns.values[coluna_lado_destino]
             estado_consulta_destino = sheetdestino.loc[0, coluna_lado_nome_destino]
 
-            try:
-                # Verifica a coluna da planilha de Origem
-                if estado_consulta.upper() in estados:
-                    kwargs['uforigem'] = coluna_lado_nome
+            # Verifica a coluna da planilha de Origem
+            if estado_consulta.upper() in estados:
+                kwargs.update({'uforigem': coluna_lado_nome})
 
-                # Verifica a coluna da planilha de destino
-                if estado_consulta_destino.upper() in estados:
-                    kwargs['ufdestino'] = coluna_lado_nome_destino
+            elif estado_consulta.upper() not in estados:
+                kwargs.update({'uforigem': None})
+            # Verifica a coluna da planilha de destino\
 
-            except Exception as e:
-                print('Error: ', e)
+            if estado_consulta_destino.upper() in estados:
+                kwargs.update({'ufdestino': coluna_lado_nome_destino})
+
+            elif estado_consulta_destino.upper() not in estados:
+                kwargs.update({'ufdestino': None})
 
         return func(*args, **kwargs)
 
