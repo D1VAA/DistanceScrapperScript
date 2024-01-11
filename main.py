46046c65d138a7f4836896a90d2ce5e188200ba5
D1VAA <davipare90@gmail.com>
2024-01-11 20:07:13 -0300

    feat: scrap from file able to export

--- main.py
+++
@@ -1,8 +0,0 @@
-from src.modules.sheet_handler import SheetHandler
-from src.modules.scrapper import ScrapFromFile
-import asyncio
-
-
-scrap = ScrapFromFile('~/Desktop/TESTE.xlsx',option=2)
-scrap.run()
-print(scrap.distances)
